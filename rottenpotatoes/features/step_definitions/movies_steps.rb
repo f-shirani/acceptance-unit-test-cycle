d10efc18f2708b03c29c769d5cccf3e205da4032
EC2 Default User <ec2-user@ip-172-31-38-211.us-east-2.compute.internal>
2021-03-10 06:33:56 +0000

    part1 & 2

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -22,4 +22,12 @@ Then /I should see all the movies/ do
   Movie.all.each do |movie|
     step %{I should see "#{movie.title}"}
   end
+
+end
+
+Then /the director of "(.+)" should be "(.+)"/ do |movie_title, director|
+  # Movie.where(title: movie_title).first.director == director_name     #use for local testing
+  movie = Movie.find_by(title: movie_title)
+  visit movie_path(movie)
+  expect(page.body).to match(/Director:\s*#{director}/)    #find director name in view of movie
 end
